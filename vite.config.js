f33c1d9c68ef430848554a3b1775af9b9157112b
mwarith <mwarith25@gmail.com>
2026-02-16 00:36:12 +0200

    some major updates

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    allowedHosts: 'all'
+  },
   plugins: [react(), tailwindcss()],
 })
